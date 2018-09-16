d9848a11a8e4154325e51af795d7c695623aa8df
Sebastian <wilk85@gmail.com>
2018-09-16 11:19:30 +0200

    js funkcje / skrócenie kodu

--- Projekt-waluty/js/main.js
+++ Projekt-waluty/js/main.js
@@ -1,220 +1,167 @@
 $(document).ready(function(){
 
+    //
     //złoto
-    $.getJSON('https://api.nbp.pl/api/cenyzlota/last/30/?format=json', function(data){
-        // console.log(data);
-        // console.log(' ' + data[0].data + ' <> : ' + data[0].cena);
-        // console.log(' ' + data[9].data + ' <> : ' + data[9].cena);
-        // console.log(' ' + data[19].data + ' <> : ' + data[19].cena);
-        // console.log(' ' + data[29].data + ' <> : ' + data[29].cena);
-        $('#gold-price').html(data[29].cena.toFixed(2));
-        // $('#l-gold30').html(`
-        // ${data[0].data} - ${data[0].cena.toFixed(2)} <br />
-        // ${data[1].data} - ${data[1].cena.toFixed(2)} <br />
-        // ${data[9].data} - ${data[9].cena.toFixed(2)} <br />
-        // ${data[10].data} - ${data[10].cena.toFixed(2)} <br />
-        // ${data[19].data} - ${data[19].cena.toFixed(2)} <br />
-        // ${data[20].data} - ${data[20].cena.toFixed(2)} <br />
-        // ${data[28].data} - ${data[28].cena.toFixed(2)} <br />
-        // ${data[29].data} - ${data[29].cena.toFixed(2)} <br />
     
-        // `)
-        //rzad 1
-        $('#row1date').html(data[22].data);
-        $('#row1price').html(data[22].cena.toFixed(2));
-        //rzad 2
-        $('#row2date').html(data[23].data);
-        $('#row2price').html(data[23].cena.toFixed(2));
-        //rzad 3
-        $('#row3date').html(data[24].data);
-        $('#row3price').html(data[24].cena.toFixed(2));
-        //rzad 4
-        $('#row4date').html(data[25].data);
-        $('#row4price').html(data[25].cena.toFixed(2));
-        //rzad 5
-        $('#row5date').html(data[26].data);
-        $('#row5price').html(data[26].cena.toFixed(2));
-        //rzad 6
-        $('#row6date').html(data[27].data);
-        $('#row6price').html(data[27].cena.toFixed(2));
-        //rzad 7
-        $('#row7date').html(data[28].data);
-        $('#row7price').html(data[28].cena.toFixed(2));
-        //rzad 8
-        $('#row8date').html(data[29].data);
-        $('#row8price').html(data[29].cena.toFixed(2));
+    $.getJSON('http://api.nbp.pl/api/cenyzlota', function(gold){
+        $('#row0price').html(gold[0].cena);
+        // console.log(gold);
+    });
+    function goldPrice(id, nr){
+        $.getJSON('https://api.nbp.pl/api/cenyzlota/last/30/?format=json', function(data1){
+            $('#row' +id+ 'date').html(data1[nr].data);
+            $('#row' +id+ 'price').html(data1[nr].cena.toFixed(2));
+        }
+    )};
+    
+    goldPrice(1, 22);
+    goldPrice(2, 23);
+    goldPrice(3, 24);
+    goldPrice(4, 25);
+    goldPrice(5, 26);
+    goldPrice(6, 27);
+    goldPrice(7, 28);
+    goldPrice(8, 29);
 
-     
+    // koniec złota
 
-    });
-    // console.log('update');
+    // waluty
 
+    function curr_val(curr_name, id){
+        $.getJSON('https://api.nbp.pl/api/exchangerates/rates/A/'+curr_name , function(data){
+            $('#'+id+'right').html(data.rates[0].mid.toFixed(3));
+        });
+    };
 
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/A/EUR/', function(eur){
-        // console.log(eur.rates[0].mid.toFixed(3)+'eur'); 
-        $('#1right').html(eur.rates[0].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/A/USD/', function(usd){
-        // console.log(usd.rates[0].mid.toFixed(3)+'usd'); 
-        $('#2right').html(usd.rates[0].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/A/GBP/', function(gbp){
-        // console.log(gbp.rates[0].mid.toFixed(3)+'gbp'); 
-        $('#3right').html(gbp.rates[0].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/A/CHF/', function(chf){
-        // console.log(chf.rates[0].mid.toFixed(3)+'chf'); 
-        $('#4right').html(chf.rates[0].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/A/CAD/', function(cad){
-        // console.log(cad.rates[0].mid.toFixed(3)+'cad'); 
-        $('#5right').html(cad.rates[0].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/A/rub/', function(rub){
-        // console.log(rub.rates[0].mid.toFixed(3)+'rub'); 
-        $('#6right').html(rub.rates[0].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/A/cny/', function(cny){
-        // console.log(cny.rates[0].mid.toFixed(3)+'cny'); 
-        $('#7right').html(cny.rates[0].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/A/nok/', function(nok){
-        // console.log(nok.rates[0].mid.toFixed(3)+'nok'); 
-        $('#8right').html(nok.rates[0].mid.toFixed(3));
-    });
+    curr_val('EUR', 1);
+    curr_val('USD', 2);
+    curr_val('GBP', 3);
+    curr_val('CHF', 4);
+    curr_val('CAD', 5);
+    curr_val('RUB', 6);
+    curr_val('CNY', 7);
+    curr_val('NOK', 8);
+
+    //koniec walut
 
     // tooltipy waluty
 
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/a/eur/last/10/?format=json', function(eur2){
-        // console.log(data3.rates[8].mid);
-        $('#tt-1').html(eur2.rates[8].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/a/usd/last/10/?format=json', function(usd2){
-        // console.log(data3.rates[8].mid);
-        $('#tt-2').html(usd2.rates[8].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/a/gbp/last/10/?format=json', function(gbp2){
-        // console.log(data3.rates[8].mid);
-        $('#tt-3').html(gbp2.rates[8].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/a/chf/last/10/?format=json', function(chf2){
-        // console.log(data3.rates[8].mid);
-        $('#tt-4').html(chf2.rates[8].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/a/cad/last/10/?format=json', function(cad2){
-        // console.log(data3.rates[8].mid);
-        $('#tt-5').html(cad2.rates[8].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/a/rub/last/10/?format=json', function(rub2){
-        // console.log(data3.rates[8].mid);
-        $('#tt-6').html(rub2.rates[8].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/a/cny/last/10/?format=json', function(cny2){
-        // console.log(data3.rates[8].mid);
-        $('#tt-7').html(cny2.rates[8].mid.toFixed(3));
-    });
-    $.getJSON('https://api.nbp.pl/api/exchangerates/rates/a/nok/last/10/?format=json', function(nok2){
-        // console.log(data3.rates[8].mid);
-        $('#tt-8').html(nok2.rates[8].mid.toFixed(3));
-    });
+    function curr_hist(curr_name, id){
+        $.getJSON('https://api.nbp.pl/api/exchangerates/rates/a/'+curr_name+ '/last/1/?format=json', function(data3){
+            $('#tt-'+id).html(data3.rates[0].mid.toFixed(3));
+        });
+    };
+
+    curr_hist('eur', 1);
+    curr_hist('usd', 2);
+    curr_hist('gbp', 3);
+    curr_hist('chf', 4);
+    curr_hist('cad', 5);
+    curr_hist('rub', 6);
+    curr_hist('cny', 7);
+    curr_hist('nok', 8);
+    
+    //koniec tooltipy waluty
+  
     //kryptowaluty
 
-    $.getJSON('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=PLN', function(btc){
-        // console.log(btc.PLN.toFixed(2)+'btc');
-        $('#btc').html(btc.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/price?fsym=BCH&tsyms=PLN', function(bch){
-        // console.log(bch.PLN.toFixed(2)+'bch');
-        $('#bch').html(bch.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=PLN', function(eth){
-        // console.log(eth.PLN.toFixed(2)+'eth');
-        $('#eth').html(eth.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/price?fsym=DASH&tsyms=PLN', function(dash){
-        // console.log(dash.PLN.toFixed(2)+'dash');
-        $('#dash').html(dash.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/price?fsym=ZEC&tsyms=PLN', function(zec){
-        // console.log(zec.PLN.toFixed(2)+'zec');
-        $('#zec').html(zec.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/price?fsym=XMR&tsyms=PLN', function(xmr){
-        // console.log(ltc.PLN.toFixed(2)+'ltc');
-        $('#xmr').html(xmr.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=PLN', function(ltc){
-        // console.log(neo.PLN.toFixed(2)+'neo');
-        $('#ltc').html(ltc.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/price?fsym=NEO&tsyms=PLN', function(neo){
-        // console.log(btg.PLN.toFixed(2)+'btg');
-        $('#neo').html(neo.PLN.toFixed(2));
-    });
+    function crypt_val(crypt_name, id){
+        $.getJSON('https://min-api.cryptocompare.com/data/price?fsym=' +crypt_name+ '&tsyms=PLN', function(data){
+            $('#'+id).html(data.PLN.toFixed(2));
+        });
+    };
+
+    crypt_val('BTC', 'btc');
+    crypt_val('BCH', 'bch');
+    crypt_val('ETH', 'eth');
+    crypt_val('DASH', 'dash');
+    crypt_val('ZEC', 'zec');
+    crypt_val('XMR', 'xmr');
+    crypt_val('LTC', 'ltc');
+    crypt_val('NEO', 'neo');
+
+    //koniec kryptowaluty
     
     // tooltipy kryptowaluty
 
-    $.getJSON('https://min-api.cryptocompare.com/data/dayAvg?fsym=BTC&tsym=PLN', function(btc2){
-        // console.log(data3.PLN);
-        $('#tt-11').html(btc2.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/dayAvg?fsym=BCH&tsym=PLN', function(bch2){
-        // console.log(data3.PLN);
-        $('#tt-12').html(bch2.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/dayAvg?fsym=ETH&tsym=PLN', function(eth2){
-        // console.log(data3.PLN);
-        $('#tt-13').html(eth2.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/dayAvg?fsym=DASH&tsym=PLN', function(dash2){
-        // console.log(data3.PLN);
-        $('#tt-14').html(dash2.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/dayAvg?fsym=ZEC&tsym=PLN', function(zec2){
-        // console.log(data3.PLN);
-        $('#tt-15').html(zec2.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/dayAvg?fsym=XMR&tsym=PLN', function(xmr2){
-        // console.log(data3.PLN);
-        $('#tt-16').html(xmr2.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/dayAvg?fsym=LTC&tsym=PLN', function(ltc2){
-        // console.log(data3.PLN);
-        $('#tt-17').html(ltc2.PLN.toFixed(2));
-    });
-    $.getJSON('https://min-api.cryptocompare.com/data/dayAvg?fsym=NEO&tsym=PLN', function(neo2){
-        // console.log(data3.PLN);
-        $('#tt-18').html(neo2.PLN.toFixed(2));
-    });
-
-    //Wstawić datę na stronę
-    // var d = new Date();
+    function cryptChange(cr_name, id){
+        $.getJSON('https://min-api.cryptocompare.com/data/dayAvg', { 
+            fsym: cr_name, 
+            tsym: 'PLN'
+        }, function(result){
+            $('#' + id).html(result.PLN.toFixed(2));
+        });
+        
+    };
+    cryptChange('BTC', 'tt-11');
+    cryptChange('BCH', 'tt-12');
+    cryptChange('ETH', 'tt-13');
+    cryptChange('DASH', 'tt-14');
+    cryptChange('ZEC', 'tt-15');
+    cryptChange('XMR', 'tt-16');
+    cryptChange('LTC', 'tt-17');
+    cryptChange('NEO', 'tt-18');
     
-    // $('.p-data')
+    //koniec tooltipy kryptowaluty
+ 
+    // TABY
+    //gold
 
-    ///////////// data na strone
+    function tab_gold(id){
+    $.getJSON('https://api.nbp.pl/api/cenyzlota/last/28', function(data){
+            $('#ghrow'+id).html(data[id-1].data);
+            $('#ghprice'+id).html(data[id-1].cena.toFixed(2));
+       
+        });  
+    };
 
+    tab_gold('1');
+    tab_gold('2');
+    tab_gold('3');
+    tab_gold('4');
+    tab_gold('5');
+    tab_gold('6');
+    tab_gold('7');
+    tab_gold('8');
+    tab_gold('9');
+    tab_gold('10');
+    tab_gold('11');
+    tab_gold('12');
+    tab_gold('13');
+    tab_gold('14');
+    tab_gold('15');
+    tab_gold('16');
+    tab_gold('17');
+    tab_gold('18');
+    tab_gold('19');
+    tab_gold('20');
+      
 
+    //currency
+    $.getJSON('https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=PLN&limit=3', function(data5){
 
+        // console.log(data5.Data);
+        function data_day(day_nr){
+            var fullDate = new Date(data5.Data[day_nr].time*1000).toLocaleDateString("sv-SE");
+            console.log(fullDate);
+            return fullDate;
+        };
+        
+        function price_day(price_nr){
+            var fullPrice = data5.Data[price_nr].close.toFixed(2);
+            console.log(fullPrice);
+            return fullPrice; 
+        };
+        
+        data_day(0);
+        data_day(1);
+        data_day(2);
+        data_day(3);//dzis
 
+        $('#tt-11').html(price_day(2));
+    });
+        
 
-    // //generator całej tabeli walut nalezy podac literę tabeli, a,b,c
-    // $.getJSON('https://api.nbp.pl/api/exchangerates/tables/a/', function(data){
-    //     // console.log(chf.rates[0].mid.toFixed(3)); 
-    //     console.log(data[0].rates);
-    //     // $('#currentDolarVal').html(parseFloat(waluta.USD_PLN.val).toFixed(3));
-    // });
-
-
-
-    // $.getJSON('https://free.currencyconverterapi.com/api/v6/convert?q=USD_PLN&compact=y', function(usd){
-    //     console.log(usd);
-    //     $('#1right').html(parseFloat(usd.USD_PLN.val).toFixed(3));
-    // });
-
-    // $.getJSON('https://free.currencyconverterapi.com/api/v6/convert?q=GBP_PLN&compact=y', function(gbp){
-    //     // console.log(gbp);
-    // })
     // setInterval(goldData, 10*(6*360000));
     
     
