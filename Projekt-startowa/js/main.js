97e18ca3ae6ee9e823ca88fd2e14eecc6b0d1554
Sebastian <wilk85@gmail.com>
2018-09-21 20:54:33 +0200

    ss

--- Projekt-startowa/js/main.js
+++ Projekt-startowa/js/main.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 let submitButton = document.getElementById('submit1');
+
 // let box1 = document.getElementById('box-1');
 // let box2 = document.getElementById('box-2');
 // let box3 = document.getElementById('box-3');
@@ -15,11 +16,14 @@ let inputWindow = document.getElementById('input1').addEventListener('input', fu
     };    
 });
 
+document.getElementById('arrowr').addEventListener('click', ()=>{
+    document.getElementById('box-container').classList.toggle('none');
+});
 
 // OBSŁUGA PRZYCISKÓW
 const change = (box_id, img_nr)=>{ //box_id 1-8 / img_nr 1-18
     document.getElementById('box-'+box_id).addEventListener('click', ()=>{
-    document.body.style.backgroundImage = "url('../img/img"+img_nr+".jpg')";
+    document.body.style.backgroundImage = "url('./img/img"+img_nr+".jpg')";
     });
 };
 
@@ -32,9 +36,33 @@ change(6, 2);
 change(7, 4);
 change(8, 3);
 
-document.getElementById('arrowr').addEventListener('click', ()=>{
-    document.getElementById('box-container').classList.toggle('none');
-});
+
+// function getCity(city){
+//     $.getJSON('https://api.weatherbit.io/v2.0/current?&city='+ city +'&country=PL&lang=pl&key=8e630ea59f8b4e0cb5d8c3389eb7c6f0', function(pogoda){
+//         // console.log(pogoda);
+//         console.log(pogoda.data[0].city_name); //miasto *
+//         // console.log(pogoda.data[0].sunrise); //wschód
+//         // console.log(pogoda.data[0].sunset); //zachód
+//         // console.log(pogoda.data[0].pres); //cisnienie hpa
+//         // console.log(pogoda.data[0].app_temp); //odczuwalna
+//         console.log(pogoda.data[0].temp); //aktualna temp *
+//         // console.log(pogoda.data[0].weather.description); //opis *
+//         // console.log((pogoda.data[0].wind_spd.toFixed(1))); //prędkosć wiatru *
+       
+//         document.getElementById('temp').innerHTML = pogoda.data[0].temp;
+//     });
+// };
+
+// getCity('cracow');
+
+
+
+
+// obliczanie prędkości wiatru z m/s na km/h
+//(value/1000).toFixed(2)*3600
+
+
+
 
 // let subYes = submitButton.disabled = true;
 
