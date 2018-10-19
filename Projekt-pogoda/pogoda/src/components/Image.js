af398eee721247eb77d68a3d72e404cc282da9be
Sebastian <wilk85@gmail.com>
2018-10-19 14:57:35 +0200

    tak

---
+++ Projekt-pogoda/pogoda/src/components/Image.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Image = () => (
+    <div className='heading-container'>
+        <h1 className='title-h1'>Apka pogodowa</h1>
+        <h3 className='title-h3'>Sprawdź pogodę w mieście które chcesz odwiedzieć</h3>
+    </div>
+);
+      
+     
+export default Image;
